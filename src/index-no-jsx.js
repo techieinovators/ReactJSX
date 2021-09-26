163fa5f08dcea1a5a172309c840828e84aae0b72
techieinovators <61652126+techieinovators@users.noreply.github.com>
2021-09-26 13:09:20 +0530

    Delete index-no-jsx.js

--- src/index-no-jsx.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = React.createElement('h1', {}, 'Content without JSX');
-ReactDOM.render(element, document.getElementById('root'));
